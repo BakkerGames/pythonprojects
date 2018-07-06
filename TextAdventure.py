844e859f4dfda5a8dda156465a5b012bc1430fb6
Scott Bakker <scott@bakkergames.com>
2018-07-05 23:34:42 -0400

    starting game

---
+++ TextAdventure.py
@@ -0,0 +1,6 @@
+# TextAdventure.py - 07/05/2018
+
+import TextAdventure_Data as d
+
+print(d.roomdesc[0])
+answer = input()
---
+++ TextAdventure_Data.py
@@ -0,0 +1,13 @@
+# TextAdventure_Data.py - 07/05/2018
+
+roomexit = []
+roomexit += [[0,0,0,0,0,0]]
+roomexit += [[2,0,0,0,0,0]]
+roomexit += [[0,1,0,0,0,0]]
+
+roomdesc = []
+roomdesc += ["Nowhere"]
+roomdesc += ["Entry"]
+roomdesc += ["Alcove"]
+
+directions = ['n','s','e','w','u','d']
